b0063a4415a65fb5d4b0a3187ecbad77d22520ad
Anton Stukach <anton.stukach@gmail.com>
2022-06-01 17:19:46 +0300

    repaire redoc

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -21,11 +21,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
+    #path(
+    #    'redoc/',
+    #    TemplateView.as_view(template_name='redoc.html'),
+    #    name='redoc'
+    #),
     re_path(
         r'^swagger(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0), name='schema-json'
@@ -33,5 +33,9 @@ urlpatterns = [
     re_path(
         r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'
+    ),
+    re_path(
+        r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
+        name='schema-redoc'
     ),
 ]
